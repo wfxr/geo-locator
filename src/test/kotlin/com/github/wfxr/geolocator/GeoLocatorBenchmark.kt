f33ab339807cedd724477f53409b5446c4bd05c2
Wenxuan <wenxuangm@gmail.com>
2018-09-12 15:15:32 +0800

    Refactor test.

--- src/test/kotlin/com/github/wfxr/geolocator/GeoLocatorBenchmark.kt
+++ src/test/kotlin/com/github/wfxr/geolocator/GeoLocatorBenchmark.kt
@@ -1,6 +1,7 @@
 package com.github.wfxr.geolocator
 
 import com.github.wfxr.geolocator.utils.loadDistrictsGaode
+import org.apache.commons.lang3.Validate
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
@@ -11,21 +12,28 @@ import java.util.*
 internal class GeoLocatorBenchmark {
     companion object {
         val geoLocator: GeoLocator = GeoLocator(loadDistrictsGaode(Paths.get("scripts/districts")))
-        val rand = Random()
+        private val rand = Random()
+
         @Suppress("unused")
         @JvmStatic
         fun geoRange() = listOf(
             Arguments.of("全球区域", Pair(-90.0, 90.0), Pair(-180.0, 180.0)),
             Arguments.of("中国外矩形区域", Pair(0.0, 73.0), Pair(55.0, 138.0)),
             Arguments.of("中国内部区域", Pair(30.0, 41.0), Pair(109.0, 119.0)))
+
+        fun randomDouble(startInclusive: Double, endInclusive: Double): Double {
+            Validate.isTrue(endInclusive >= startInclusive, "Start value must be smaller or equal to end value.")
+            return if (startInclusive == endInclusive) startInclusive
+            else startInclusive + (endInclusive - startInclusive) * rand.nextDouble()
+        }
     }
 
     @ParameterizedTest
     @MethodSource("geoRange")
     fun benchLocate(remark: String, latRange: Pair<Double, Double>, lonRange: Pair<Double, Double>) {
         repeat(1_000_000) {
-            val lat = rand.nextDouble() * (latRange.second - latRange.first) + latRange.first
-            val lon = rand.nextDouble() * (lonRange.second - lonRange.first) + lonRange.first
+            val lat = randomDouble(latRange.first, latRange.second)
+            val lon = randomDouble(lonRange.first, lonRange.second)
             geoLocator.locate(WGSPoint(lat, lon))
         }
     }
@@ -35,8 +43,8 @@ internal class GeoLocatorBenchmark {
     @MethodSource("geoRange")
     fun benchFastLocate(remark: String, latRange: Pair<Double, Double>, lonRange: Pair<Double, Double>) {
         repeat(1_000_000) {
-            val lat = rand.nextDouble() * (latRange.second - latRange.first) + latRange.first
-            val lon = rand.nextDouble() * (lonRange.second - lonRange.first) + lonRange.first
+            val lat = randomDouble(latRange.first, latRange.second)
+            val lon = randomDouble(lonRange.first, lonRange.second)
             geoLocator.fastLocate(WGSPoint(lat, lon))
         }
     }
